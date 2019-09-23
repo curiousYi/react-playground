71df5e2577247988f9b2a56e0164a6fc9df6dc4d
Yi Chao <changingtimes@gmail.com>
2019-09-23 08:30:10 -0500

    React Context feature

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const ThemeContext = React.createContext('originalTheme');
+
+class App extends React.Component {
+  render() {
+    // Use a Provider to pass the current theme to the tree below.
+    // Any component can read it, no matter how deep it is.
+    // In this example, we're passing "dark" as the current value.
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+ 
+        <ThemeContext.Provider value="dark">
+          <Toolbar />
+        </ThemeContext.Provider>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/a.js
@@ -0,0 +1,5 @@
+export default class App extends React.Component {
+    render() {
+      return <Toolbar theme="dark" />;
+    }
+  }
